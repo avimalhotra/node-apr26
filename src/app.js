eb7d687097cb6fad2a724ca1dbf776b78fa0c463
Avinash Malhotra <avimalhotra505@gmail.com>
2026-05-03 11:11:51 +0530

    Passport js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import nunjucks from "nunjucks";
+import session from 'express-session';
 
 /* db */
 import mongoose from './config/dao.js';
@@ -16,16 +17,33 @@ const port=process.env.PORT || 3000;
 app.use(express.static(path.resolve("src/public")));
 app.use(express.static(path.resolve("node_modules/bootstrap/dist")));
 
+    // trust first proxy
+app.set('trust proxy', 1); 
+
+app.use(session({
+    secret:"session",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{secure:false}
+}))
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-import Admin from './controllers/admin.js';
+// import Admin from './controllers/admin.js';
 
 /* APi Routes */
 import router from './routes/api.js';
-
 app.use("/api",router);
+
+
+/* passport */
+
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+
+
 
 // configure
 nunjucks.configure(path.resolve('src/public/views'),{
@@ -34,6 +52,43 @@ nunjucks.configure(path.resolve('src/public/views'),{
     noCache:false,
     watch:true
 }); 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    done(null, user[0].id);
+  });
+passport.deserializeUser(function (user, next) {
+    next(null, user);
+});
+
+passport.use('local', new LocalStrategy((username, password, done) => {
+    
+    Admins.find({ username: username }).then(user=>{
+      if( user.length==0 ){
+          return done(null, null, { message: 'No user found!' });
+      }
+      else  if (user[0].password !== password) {
+          return done(null, null, { message: 'Password is incorrect!' });
+      }
+      else{
+          return done(null, user, null);
+      }
+      })
+
+  }
+));
+
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      res.status(403).send('Forbidden');
+    }
+}
+  
+
 
 app.get("/",(req,res)=>{
           res.status(200).render("index.njk",{
@@ -76,7 +131,6 @@ app.get("/cars/:car",(req,res)=>{
 });
 
 
-
 app.get("/addcars",(req,res)=>{
 
      const {car,price,type}=req.query;
@@ -92,11 +146,46 @@ app.get("/addcars",(req,res)=>{
 
 });
 
-app.post("/admin",Admin);
 
 app.get("/contact",(req,res)=>{
      res.status(200).render("contact.html",{title:"Contact Us"});
 });
+
+app.get('/admin', isAuthenticated, (req, res) => { res.render('admin.html',{name:"admin"}) });
+
+app.post("/login",(req,res)=>{
+     const {username,password}=req.body;
+
+     passport.authenticate('local',  (err, user, info) =>{
+          
+          if (err) {
+            res.render('login.html', { error: err });
+          } 
+          else if (!user) {
+            res.render('login.html', { errorMessage: info.message });
+          } 
+          else {
+            //setting users in session
+            req.logIn(user, function (err) {
+              if (err) {
+                res.render('login.html', { error: err });
+              } else {
+                res.render('admin.html',{ name:user[0].username});
+               }
+            })
+          }
+        })(req, res);
+});
+
+app.post('/logout', function(req, res, next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+     else{
+          res.status(200).render("logout.html",{title:"Logout"});
+     }
+  });
+});
+
 
 /* wild card */
 app.get('/*splat',(req,res)=>{
