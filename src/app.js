0858612aef0ac4c0fbb370c9ad38253d0ad9b965
Avinash Malhotra <avimalhotra505@gmail.com>
2026-05-10 10:25:47 +0530

    app ready

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import nunjucks from "nunjucks";
 import session from 'express-session';
 
 /* db */
-import mongoose from './config/dao.js';
-import CarsSchema from './models/Suzuki.js';
-import AdminSchema from './models/Admin.js';
-const Cars=mongoose.model("Cars",CarsSchema);
-const Admins=mongoose.model("Admins",AdminSchema);
+// import mongoose from './config/dao.js';
+// import CarsSchema from './models/Suzuki.js';
+// import AdminSchema from './models/Admin.js';
+// const Cars=mongoose.model("Cars",CarsSchema);
+// const Admins=mongoose.model("Admins",AdminSchema);
 
 import helmet from 'helmet';
 import cors from 'cors';
@@ -45,14 +45,14 @@ app.use(express.urlencoded({ extended: true }));
 // import Admin from './controllers/admin.js';
 
 /* APi Routes */
-import router from './routes/api.js';
-app.use("/api",router);
+// import router from './routes/api.js';
+// app.use("/api",router);
 
 
 /* passport */
 
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
+// import passport from 'passport';
+// import { Strategy as LocalStrategy } from 'passport-local';
 
 
 
@@ -64,40 +64,40 @@ nunjucks.configure(path.resolve('src/public/views'),{
     watch:true
 }); 
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.serializeUser(function (user, done) {
-    done(null, user[0].id);
-  });
-passport.deserializeUser(function (user, next) {
-    next(null, user);
-});
+// passport.serializeUser(function (user, done) {
+//     done(null, user[0].id);
+//   });
+// passport.deserializeUser(function (user, next) {
+//     next(null, user);
+// });
 
-passport.use('local', new LocalStrategy((username, password, done) => {
+// passport.use('local', new LocalStrategy((username, password, done) => {
     
-    Admins.find({ username: username }).then(user=>{
-      if( user.length==0 ){
-          return done(null, null, { message: 'No user found!' });
-      }
-      else  if (user[0].password !== password) {
-          return done(null, null, { message: 'Password is incorrect!' });
-      }
-      else{
-          return done(null, user, null);
-      }
-      })
+//     Admins.find({ username: username }).then(user=>{
+//       if( user.length==0 ){
+//           return done(null, null, { message: 'No user found!' });
+//       }
+//       else  if (user[0].password !== password) {
+//           return done(null, null, { message: 'Password is incorrect!' });
+//       }
+//       else{
+//           return done(null, user, null);
+//       }
+//       })
 
-  }
-));
+//   }
+// ));
 
-function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.status(403).send('Forbidden');
-    }
-}
+// function isAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//       next();
+//     } else {
+//       res.status(403).send('Forbidden');
+//     }
+// }
   
 
 
@@ -119,83 +119,83 @@ app.get("/login",(req,res)=>{
      res.status(200).render("login.html",{title:"Login"});
 });
 
-app.get("/cars",(req,res)=>{
-      Cars.find({}).select('name type price -_id').then(i=>{
-          res.status(200).render("cars.njk",{title:"All Cars", cars:i});
-      }).catch(e=>{
-          console.warn(e);
-     })
-});
+// app.get("/cars",(req,res)=>{
+//       Cars.find({}).select('name type price -_id').then(i=>{
+//           res.status(200).render("cars.njk",{title:"All Cars", cars:i});
+//       }).catch(e=>{
+//           console.warn(e);
+//      })
+// });
 
-app.get("/cars/:car",(req,res)=>{
-     const car=req.params.car.replaceAll("-"," ");
+// app.get("/cars/:car",(req,res)=>{
+//      const car=req.params.car.replaceAll("-"," ");
 
-      Cars.find({name:car}).select("-_id").then(i=>{
-          if(i.length){
-               res.status(200).render("car.njk",{title:car, car:i});
-          }
-          else{
-               res.status(200).render("car.njk",{title:"No Car Found"});
-          }
-      });
+//       Cars.find({name:car}).select("-_id").then(i=>{
+//           if(i.length){
+//                res.status(200).render("car.njk",{title:car, car:i});
+//           }
+//           else{
+//                res.status(200).render("car.njk",{title:"No Car Found"});
+//           }
+//       });
      
-});
+// });
 
 
-app.get("/addcars",(req,res)=>{
+// app.get("/addcars",(req,res)=>{
 
-     const {car,price,type}=req.query;
+//      const {car,price,type}=req.query;
 
-     const newCar=new Cars({
-          _id: new mongoose.Types.ObjectId(),
-          name:car,
-          type:type,
-          price:price
-     });
+//      const newCar=new Cars({
+//           _id: new mongoose.Types.ObjectId(),
+//           name:car,
+//           type:type,
+//           price:price
+//      });
 
-     newCar.save().then(i=>res.status(200).send(`${i} saved successfully`)).catch(err=>res.status(400).send(`Error while saving data : ${err}`));
+//      newCar.save().then(i=>res.status(200).send(`${i} saved successfully`)).catch(err=>res.status(400).send(`Error while saving data : ${err}`));
 
-});
+// });
 
 
 app.get("/contact",(req,res)=>{
      res.status(200).render("contact.html",{title:"Contact Us"});
 });
 
-app.get('/admin', isAuthenticated, (req, res) => { res.render('admin.html',{name:"admin"}) });
+// app.get('/admin', isAuthenticated, (req, res) => { res.render('admin.html',{name:"admin"}) });
 
-app.post("/login",(req,res)=>{
-     const {username,password}=req.body;
+// app.post("/login",(req,res)=>{
+//      const {username,password}=req.body;
 
-     passport.authenticate('local',  (err, user, info) =>{
+//      passport.authenticate('local',  (err, user, info) =>{
           
-          if (err) {
-            res.render('login.html', { error: err });
-          } 
-          else if (!user) {
-            res.render('login.html', { errorMessage: info.message });
-          } 
-          else {
-            //setting users in session
-            req.logIn(user, function (err) {
-              if (err) {
-                res.render('login.html', { error: err });
-              } else {
-                res.render('admin.html',{ name:user[0].username});
-               }
-            })
-          }
-        })(req, res);
-});
+//           if (err) {
+//             res.render('login.html', { error: err });
+//           } 
+//           else if (!user) {
+//             res.render('login.html', { errorMessage: info.message });
+//           } 
+//           else {
+//             //setting users in session
+//             req.logIn(user, function (err) {
+//               if (err) {
+//                 res.render('login.html', { error: err });
+//               } else {
+//                 res.render('admin.html',{ name:user[0].username});
+//                }
+//             })
+//           }
+//         })(req, res);
+// });
 
-app.post('/logout', function(req, res, next){
-  req.logout(function(err) {
-    if (err) { return next(err); }
-     else{
-          res.status(200).render("logout.html",{title:"Logout"});
-     }
-  });
-});
+// app.post('/logout', function(req, res, next){
+//   req.logout(function(err) {
+//     if (err) { return next(err); }
+//      else{
+//           res.status(200).render("logout.html",{title:"Logout"});
+//      }
+//   });
+// });
 
 
 /* wild card */
